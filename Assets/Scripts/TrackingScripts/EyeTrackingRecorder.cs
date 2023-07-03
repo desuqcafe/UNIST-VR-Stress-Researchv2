ee5c296718419f132a03707f42d1edf5d6a03c61
Trinh Ngo <trinhngo.ginger@gmail.com>
2023-07-04 07:13:19 +0900

    add hit features, add eye transform inputs to the EyeTrackingRecord

--- Assets/Scripts/TrackingScripts/EyeTrackingRecorder.cs
+++ Assets/Scripts/TrackingScripts/EyeTrackingRecorder.cs
@@ -27,12 +27,16 @@ public class EyeTrackingRecorder : MonoBehaviour
 
     private string folderPath;
     private string timeStamp;
+    private string hitDataFilePath;
+    private string faceExpressionDataFilePath;
     private string eyeTrackingDataFilePath;
     private string headsetAndControllerDataFilePath;
     private string headsetVelocityAndAccelerationDataFilePath;
     public string currentTask { get; set; }
 
     // Buffer to store data before writing to files
+    private List<string> hitDataBuffer = new List<string>();
+    private List<string> faceExpressionDataBuffer = new List<string>();
     private List<string> eyeTrackingDataBuffer = new List<string>();
     private List<string> headsetAndControllerDataBuffer = new List<string>();
     private List<string> headsetVelocityAndAccelerationDataBuffer = new List<string>();
@@ -44,7 +48,6 @@ public class EyeTrackingRecorder : MonoBehaviour
 
     private void Awake()
     {
-
         if (Instance == null)
         {
             Instance = this;
@@ -56,6 +59,8 @@ public class EyeTrackingRecorder : MonoBehaviour
         folderPath = Application.persistentDataPath;
         timeStamp = System.DateTime.Now.ToString("HH-mm-ss");
 
+        hitDataFilePath = Path.Combine(folderPath, "hitData_" + timeStamp + ".csv");
+        faceExpressionDataFilePath = Path.Combine(folderPath, "faceExpressionData_" + timeStamp + ".csv");
         eyeTrackingDataFilePath = Path.Combine(folderPath, "eyeTrackingData_" + timeStamp + ".csv");
         headsetAndControllerDataFilePath = Path.Combine(folderPath, "headsetAndControllerData_" + timeStamp + ".csv");
         headsetVelocityAndAccelerationDataFilePath = Path.Combine(folderPath, "headsetVelocityAndAccelerationData_" + timeStamp + ".csv");
@@ -73,11 +78,32 @@ public class EyeTrackingRecorder : MonoBehaviour
         }
     }
 
+    // Hit position and time data
+    // public LayerMask trackingLayerMask;
+    private RaycastHit hit;
+    private Dictionary<GameObject, float> startTimeForObjects = new Dictionary<GameObject, float>();
+    float duration;
+    Vector3 hitPosition;
+    GameObject hitObject;
+    // Reference to OVRFaceExpressions script
+    [SerializeField]
+    private OVRFaceExpressions faceExpressions;
+    private class FaceWeightComponent
+    {
+        public string Name;
+        public float Weight;
+        public OVRFaceExpressions.FaceExpression FaceExpression { get; set; }
 
+        public override string ToString() => $"{FaceExpression}: {Name}: {Weight}";
+    }
+    private List<FaceWeightComponent> components = new List<FaceWeightComponent>();
 
     // Reference to the OVREyeGaze script for left and right eyes
     public OVREyeGaze leftEyeGaze;
     public OVREyeGaze rightEyeGaze;
+
+    public Transform leftEye;
+    public Transform rightEye;
 
     // Variables to store eye tracking data
     private Vector3 leftEyeGazeDirection;
@@ -111,6 +137,7 @@ public class EyeTrackingRecorder : MonoBehaviour
     void Start()
     {
         // Initialize variables
+        InitFaceExpression();
         leftEyeGazeDirection = Vector3.zero;
         rightEyeGazeDirection = Vector3.zero;
         leftEyeConfidence = 0;
@@ -121,70 +148,138 @@ public class EyeTrackingRecorder : MonoBehaviour
         previousHeadsetVelocity = Vector3.zero;
         headsetAcceleration = Vector3.zero;
     }
-
+    private void InitFaceExpression() 
+    {
+        foreach (OVRFaceExpressions.FaceExpression e in Enum.GetValues(typeof(OVRFaceExpressions.FaceExpression)))
+        {
+            //Create a component for each face expression features
+            FaceWeightComponent faceWeightComponent = new FaceWeightComponent();
+            faceWeightComponent.FaceExpression = e;
+            faceWeightComponent.Name = e.ToString();
+            components.Add(faceWeightComponent);
+        }
+    }
     // Update is called once per frame
     void Update()
-{
-    // Get eye tracking data from OVREyeGaze for left and right eyes
-    leftEyeGazeDirection = leftEyeGaze.transform.forward;
-    leftEyeConfidence = leftEyeGaze.Confidence;
-    rightEyeGazeDirection = rightEyeGaze.transform.forward;
-    rightEyeConfidence = rightEyeGaze.Confidence;
-    leftEyePosition = leftEyeGaze.transform.position;
-    rightEyePosition = rightEyeGaze.transform.position;
-    leftEyeRotation = leftEyeGaze.transform.rotation;
-    rightEyeRotation = rightEyeGaze.transform.rotation;
-
-    // Buffer eye tracking data
-    BufferEyeTrackingData();
-
-    // Get headset and controller positions and rotations
-    headsetPosition = headsetTransform.position;
-    headsetRotation = headsetTransform.rotation;
-    leftControllerPosition = leftControllerTransform.position;
-    leftControllerRotation = leftControllerTransform.rotation;
-    rightControllerPosition = rightControllerTransform.position;
-    rightControllerRotation = rightControllerTransform.rotation;
-
-    // Buffer headset and controller data
-    BufferHeadsetAndControllerData();
-
-    // Calculate headset velocity and acceleration
-    Vector3 currentHeadsetPosition = headsetTransform.position;
-    headsetVelocity = (currentHeadsetPosition - previousHeadsetPosition) / Time.deltaTime;
-    headsetAcceleration = (headsetVelocity - previousHeadsetVelocity) / Time.deltaTime;
-
-    // Buffer headset velocity and acceleration data
-    BufferHeadsetVelocityAndAccelerationData();
-
-    // Update previous position and velocity values
-    previousHeadsetPosition = currentHeadsetPosition;
-    previousHeadsetVelocity = headsetVelocity;
-
-    frameCounter++;
-
-    if (frameCounter >= writeInterval)
     {
-        // Create new lists that are copies of the original lists and pass those to WriteDataToFileAsync
-        var eyeTrackingDataToWrite = new List<string>(eyeTrackingDataBuffer);
-        WriteDataToFileAsync(eyeTrackingDataFilePath, eyeTrackingDataToWrite);
+        // hit data
+        // trackingLayerMask = LayerMask.GetMask("TrackingLayer");
+        if (Physics.Raycast(leftEye.transform.position, leftEye.transform.forward, out hit, 10) ||
+            Physics.Raycast(rightEye.transform.position, rightEye.transform.forward, out hit, 10)) {
+            // filePathText.text +=  "\n\nHit Position:" + hit.point.ToString();
+            hitObject = hit.collider.gameObject;
+            // filePathText.text += "\nHit Object:" + hitObject;
+            
+            hitPosition = hit.point;
+            if (!startTimeForObjects.ContainsKey(hitObject)) {
+                // This is the first time the object is looked at and store current time
+                startTimeForObjects[hitObject] = Time.time;
+            } else {
+                // The object is being looked at and calculate the duration
+                duration = Time.time - startTimeForObjects[hitObject];
+                // filePathText.text += "\nObject: " + hitObject.name + ", Duration: " + duration; // secs: duration%60; mins: duration/60 
+            }
+        }
+        // Buffer hit data
+        BufferHitData();
 
-        var headsetAndControllerDataToWrite = new List<string>(headsetAndControllerDataBuffer);
-        WriteDataToFileAsync(headsetAndControllerDataFilePath, headsetAndControllerDataToWrite);
+        // Get face expression data from OVRFaceExpressions.cs
+        foreach (FaceWeightComponent comp in components)
+        {
+            float weight;
+            // filePathText.text += "- Num " + frameCounter + ":" + "\n" ;
+            // filePathText.text += comp.FaceExpression + "\n";
+            if (faceExpressions.TryGetFaceExpressionWeight(comp.FaceExpression, out weight))
+            {
+                // filePathText.text += weight;
+                comp.Weight = weight;
+            }
+        }
+        // Buffer face expression data
+        BufferFaceExpressionData();
 
-        var headsetVelocityAndAccelerationDataToWrite = new List<string>(headsetVelocityAndAccelerationDataBuffer);
-        WriteDataToFileAsync(headsetVelocityAndAccelerationDataFilePath, headsetVelocityAndAccelerationDataToWrite);
+        // Get eye tracking data from OVREyeGaze for left and right eyes
+        leftEyeGazeDirection = leftEyeGaze.transform.forward;
+        leftEyeConfidence = leftEyeGaze.Confidence;
+        rightEyeGazeDirection = rightEyeGaze.transform.forward;
+        rightEyeConfidence = rightEyeGaze.Confidence;
+        leftEyePosition = leftEyeGaze.transform.position;
+        rightEyePosition = rightEyeGaze.transform.position;
+        leftEyeRotation = leftEyeGaze.transform.rotation;
+        rightEyeRotation = rightEyeGaze.transform.rotation;
 
-        // Clear the buffers
-        eyeTrackingDataBuffer.Clear();
-        headsetAndControllerDataBuffer.Clear();
-        headsetVelocityAndAccelerationDataBuffer.Clear();
+        // Buffer eye tracking data
+        BufferEyeTrackingData();
 
-        // Reset the frame counter
-        frameCounter = 0;
+        // Get headset and controller positions and rotations
+        headsetPosition = headsetTransform.position;
+        headsetRotation = headsetTransform.rotation;
+        leftControllerPosition = leftControllerTransform.position;
+        leftControllerRotation = leftControllerTransform.rotation;
+        rightControllerPosition = rightControllerTransform.position;
+        rightControllerRotation = rightControllerTransform.rotation;
+
+        // Buffer headset and controller data
+        BufferHeadsetAndControllerData();
+
+        // Calculate headset velocity and acceleration
+        Vector3 currentHeadsetPosition = headsetTransform.position;
+        headsetVelocity = (currentHeadsetPosition - previousHeadsetPosition) / Time.deltaTime;
+        headsetAcceleration = (headsetVelocity - previousHeadsetVelocity) / Time.deltaTime;
+
+        // Buffer headset velocity and acceleration data
+        BufferHeadsetVelocityAndAccelerationData();
+
+        // Update previous position and velocity values
+        previousHeadsetPosition = currentHeadsetPosition;
+        previousHeadsetVelocity = headsetVelocity;
+
+        frameCounter++;
+
+        if (frameCounter >= writeInterval)
+        {
+            // Create new lists that are copies of the original lists and pass those to WriteDataToFileAsync
+            var hitDataToWrite = new List<string>(hitDataBuffer);
+            WriteDataToFileAsync(hitDataFilePath, hitDataToWrite);
+
+            var faceExpressionDataToWrite = new List<string>(faceExpressionDataBuffer);
+            WriteDataToFileAsync(faceExpressionDataFilePath, faceExpressionDataToWrite);
+
+            var eyeTrackingDataToWrite = new List<string>(eyeTrackingDataBuffer);
+            WriteDataToFileAsync(eyeTrackingDataFilePath, eyeTrackingDataToWrite);
+
+            var headsetAndControllerDataToWrite = new List<string>(headsetAndControllerDataBuffer);
+            WriteDataToFileAsync(headsetAndControllerDataFilePath, headsetAndControllerDataToWrite);
+
+            var headsetVelocityAndAccelerationDataToWrite = new List<string>(headsetVelocityAndAccelerationDataBuffer);
+            WriteDataToFileAsync(headsetVelocityAndAccelerationDataFilePath, headsetVelocityAndAccelerationDataToWrite);
+
+            // Clear the buffers
+            eyeTrackingDataBuffer.Clear();
+            headsetAndControllerDataBuffer.Clear();
+            headsetVelocityAndAccelerationDataBuffer.Clear();
+
+            // Reset the frame counter
+            frameCounter = 0;
+        }
     }
-}
+    // Function to buffer face expression data
+    void BufferHitData()
+    {
+        string hitData = $"{Time.time}, {currentTask}, {hitObject.name}, {hitPosition}, {duration}";
+        hitDataBuffer.Add(hitData);
+    }
 
+    // Function to buffer face expression data
+    void BufferFaceExpressionData()
+    {
+        string faceExpressionData = $"{Time.time}, {currentTask}";
+        foreach (FaceWeightComponent comp in components)
+        {
+            faceExpressionData += $", {comp.Weight}";
+        }
+        faceExpressionDataBuffer.Add(faceExpressionData);
+    }
 
     // Function to buffer eye tracking data
     void BufferEyeTrackingData()
